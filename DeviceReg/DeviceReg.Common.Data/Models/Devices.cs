7d21b8212d8438910e787cb00c02d48ae51ffdc4
Adam Sasin <adamsasin95@gmail.com>
2016-11-12 19:14:05 +0100

    initial commit

---
+++ DeviceReg/DeviceReg.Common.Data/Models/Devices.cs
@@ -0,0 +1,20 @@
+﻿using DeviceReg.Common.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceReg.Common.Data.Models
+{
+    public class Device: IEntity
+    {
+        public Device()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DeviceReg/DeviceReg.WebApi/DeviceReg.WebApi/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DeviceReg.WebApi.Startup))]
+
+namespace DeviceReg.WebApi
+{
+    public partial class Startup
+    {
+        //Test
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
